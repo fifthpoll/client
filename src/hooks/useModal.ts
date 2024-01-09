bf7d25257627cd42291d4bbc576b84d0f283fc9d
Riya Jain <riya1jain567@gmail.com>
2024-01-10 02:52:43 +0530

    another possible fix for modal not opening

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -5,13 +5,17 @@ export default function useModal() {
   const global = useGlobalContext();
 
   function show(element: ReactNode) {
+    console.log("Showing modal with element:", element);
     if (global && global.modalState && global.modalState.setModal) {
+      console.log("Showing modal with element: Inside if", element);
+
       global.modalState.setModal(element);
     }
   }
 
   function hide() {
     if (global && global.modalState && global.modalState.setModal) {
+      console.log("Hiding modal");
       global.modalState.setModal(null);
     }
   }
