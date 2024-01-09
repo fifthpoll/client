3c0a8ac48c9ed595d5a9fa8d36ba7b2189892a49
Riya Jain <riya1jain567@gmail.com>
2024-01-10 03:08:28 +0530

    Removed unnecessary console.log statement, working fine in production mode now

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -5,19 +5,13 @@ export default function useModal() {
   const globalC = useGlobalContext();
 
   function show(element: ReactNode) {
-    console.log("Showing modal with element:", element);
-    console.log(globalC);
-    console.log(globalC.modalState);
-    console.log(globalC.modalState.setModal);
     if (globalC && globalC.modalState && globalC.modalState.setModal) {
-      console.log("Showing modal with element: Inside if", element);
       globalC.modalState.setModal(element);
     }
   }
 
   function hide() {
     if (globalC && globalC.modalState && globalC.modalState.setModal) {
-      console.log("Hiding modal");
       globalC.modalState.setModal(null);
     }
   }
